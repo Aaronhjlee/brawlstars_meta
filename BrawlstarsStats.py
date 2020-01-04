86c6881f5ddac3b9fb2e39e5ded57d6f6f447d9f
Aaron <aaronhjlee1@gmail.com>
2020-01-03 19:09:55 -0800

    api pull and 1 plot added

--- BrawlstarsStats.py
+++ BrawlstarsStats.py
@@ -3,58 +3,94 @@ import numpy as np
 import json
 from pandas.io.json import json_normalize
 
-from API_key import token
+from API_key import *
+import requests as r
 
-
-# !curl -X GET --header 'Accept: application/json' --header "authorization: Bearer "$token"" 'https://api.brawlstars.com/v1/players/%23'$player_id''
 
 '''
 Get the player information
 '''
 
-def top_200(country_code):
-    rank_raw = curl -X GET --header 'Accept: application/json' --header "authorization: Bearer "$token"" 'https://api.brawlstars.com/v1/rankings/'$country_code'/players'
-    country = json.loads(rank_raw[0] + rank_raw[1])
-    player_id = []
-    names = []
-    for i in country['items']:
-        player_id.append(i['tag'])
-        names.append(i['name'])
-    return player_id, names
+class BSdata():
 
-def gather_account_info(player_id):
-    clean_id = []
-    for i in player_id:
-        clean_id.append(i[1:])
+    def __init__(self, country_code, headers):
+        self.country_code = country_code
+        self.headers = headers
 
-    account_info = []
-    for i in clean_id:
-        temp = curl -X GET --header 'Accept: application/json' --header "authorization: Bearer "$token"" 'https://api.brawlstars.com/v1/players/%23'$i''
-        account_info.append(json.loads(temp[1]))
-    return account_info
+    def top_200(self):
+        data = r.get('https://api.brawlstars.com/v1/rankings/' + self.country_code +'/players', headers=self.headers).json()
+        player_id = []
+        names = []
+        for i in data['items']:
+            player_id.append(i['tag'])
+            names.append(i['name'])
+        return player_id, names
 
-def brawler_names():    
-    brawler_names = []
-    for i in account_info[0]['brawlers']:
-        brawler_names.append(i['name'])
-    return brawler_names
+    def gather_account_info(self):
+        player_id, names = self.top_200()
+        clean_id = []
+        for i in player_id:
+            clean_id.append(i[1:])
+            
+        account_info = []
+        for i in clean_id:
+            temp = (r.get('https://api.brawlstars.com/v1/players/%23' + i, headers=self.headers).json())
+            account_info.append(temp)
+        return account_info
 
-def top_players():    
-    trophy_200 = []
-    for i in account_info:
-        trophy = []
-        for j in i['brawlers']:
-            trophy.append(j['trophies'])
-        trophy_200.append(trophy)
-    return trophy_200
+    def brawler_names(self):
+        data = r.get('https://api.brawlstars.com/v1/brawlers', headers=headers).json()
+        brawler_names = []
+        for i in data['items']:
+            brawler_names.append(i['name'])
+        return brawler_names
 
-def makeDataframe():
-    df = pd.DataFrame(names, columns=['name'])
-    df['id'] = player_id
-    df2 = pd.DataFrame(data=trophy_200, columns=brawler_names)
-    df = pd.concat([df, df2], axis=1)
-    return df
+    def top_players(self):    
+        account_info = self.gather_account_info()
+        trophy_200 = []
+        for i in account_info:
+            trophy = []
+            for j in i['brawlers']:
+                trophy.append(j['trophies'])
+            trophy_200.append(trophy)
+        return trophy_200
 
+    def makeDataframe(self):
+        player_id, names = self.top_200()
+        brawler_names = self.brawler_names()
+        trophy_200 = self.top_players()
+        
+        df = pd.DataFrame(names, columns=['name'])
+        df['id'] = player_id
+        df2 = pd.DataFrame(data=trophy_200, columns=brawler_names)
+        df = pd.concat([df, df2], axis=1)
+        return df
+
+'''
+plotting character breakdown
+'''
+
+def plot_avg_trophies(df):
+    avg_trophies = df.describe().transpose()[['mean']].reset_index().sort_values('mean', ascending=False)
+
+    plt.figure(figsize=(16,8))
+    plt.title('Average Trophies per Character', fontsize=30)
+    plt.xlabel('Brawlers', fontsize=20)
+    plt.ylabel('Brawlers', fontsize=20)
+    
+    avg = avg_trophies.mean().values[0]
+    mask1 = avg_trophies['mean'] < avg
+    mask2 = avg_trophies['mean'] >= avg
+
+    plt.bar(avg_trophies['index'][mask2], avg_trophies['mean'][mask2], color='green')
+    plt.bar(avg_trophies['index'][mask1], avg_trophies['mean'][mask1], color='red')
+    
+    plt.ylim((550, 850))
+    plt.xticks(fontsize=14, rotation=90)
+    plt.yticks(fontsize=14)
+    
+    plt.plot(avg_trophies['index'], [avg_trophies.mean()]*(len(avg_trophies['index'])), label='Mean={}'.format(round(avg,2)), linestyle='--')
+    plt.legend(fontsize=20)
 
 
 '''
@@ -96,10 +132,11 @@ def winrateBB(log):
 
 
 if __name__ == "__main__":
-    country_code = 'US'
+    country_code = 'global'
     
-    player_id, names = top_200(country_code)
-    account_info = gather_account_info(player_id)
-    brawler_names = brawler_names()
-    trophy_200 = top_players()
-    df = makeDataframe()
+    BS = BSdata(country_code, headers)
+    df = BS.makeDataframe()
+
+
+# !curl -X GET --header 'Accept: application/json' --header "authorization: Bearer "$self.token"" 'https://api.brawlstars.com/v1/rankings/'$self.country_code'/players'
+# !curl -X GET --header 'Accept: application/json' --header "authorization: Bearer "$token"" 'https://api.brawlstars.com/v1/players/%23'$i''
